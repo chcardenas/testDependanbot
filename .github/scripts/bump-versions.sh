caca7bef0c604e9c805fe3ba711cccd4a970e6b2
ccardenas <chcardenas.ext@acciona.com>
2022-05-24 02:14:38 +0200

    feat(dependabot): add feature

--- .github/scripts/bump-versions.sh
+++ .github/scripts/bump-versions.sh
@@ -11,6 +11,7 @@ for cmt in $(git rev-list --reverse $before..$after); do
     git checkout -q $cmt
     commit_message=$(git log -1 --pretty=format:"%s")
     changes=$(git diff --name-only HEAD^ HEAD)
+    echo $changes
     msg=$(echo $commit_message | cut -d "(" -f2 | cut -d ")" -f1)
     size=$(echo $msg | wc -c)
     read -a strarr <<< "$msg"
@@ -18,37 +19,41 @@ for cmt in $(git rev-list --reverse $before..$after); do
     
     
     for change in ${changes[@]}; do
-            if [ $size -lt 2 ]; then
-                msg=$(echo - $strarr[1]) 
-            else
-                #non-zero length
-                msg=$(echo "- ***$msg***: $strarr[1] ") 
-            fi
-            msgb=$(echo "### $strarr[0] ")
-            
-            if echo $commit_message | grep -qE '(!:)|BREAKING'; then
+        echo $change
+        if [ $size -lt 2 ]; then
+           msg=$(echo - $strarr[1]) 
+        else
+           #non-zero length
+            msg=$(echo "- ***$msg***: $strarr[1] ") 
+        fi
+        msgb=$(echo "### $strarr[0] ")
+        echo $commit_message
+        if echo $commit_message | grep -qE '(!:)|BREAKING'; then
         
+           echo "major version"
+           npm version major                  
                     
-                    npm version major                  
-                    
-                    break
-            fi
+           break
+        fi
 
-            if echo $commit_msg | grep -qE '^feat'; then
-                       npm version minor
-                        
-                    break
-            fi
+        if echo $commit_msg | grep -qE '^feat'; then
+           echo "minor version"
+           npm version minor             
+           break
+        fi
 
-            if  echo $commit_msg| grep -qE '^fix'; then
-                    npm version patch
+        if  echo $commit_msg| grep -qE '^fix'; then
+            echo "patch version"
+            npm version patch
                     
-            fi
+        fi
         
     done
     
 done
+
 version=$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package.json)
+echo $version
 date=$(git show -s --format=%cd --date=short )
 msgc=$(echo "## v$version ($date) ")
 #echo "$msgc" >> $changelog_file
