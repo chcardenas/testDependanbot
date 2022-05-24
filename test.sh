62bc298725ab9ec6b2e98511f790aa7ced5fb7cb
ccardenas <chcardenas.ext@acciona.com>
2022-05-24 02:43:32 +0200

    feat(a): asdas

--- test.sh
+++ test.sh
@@ -9,18 +9,19 @@ feat=$(echo $commit_message | cut -d "(" -f2 | cut -d ")" -f1)
 echo $feat
 msg=$(echo "### $feat \n\n\n")
 size=$(echo $feat | wc -c)
-echo $msg
+echo $commit_message
 if [ $size -lt 2 ]; then
     echo "zero" 
 else
     #non-zero length
     echo "filed"
+    echo $feat
 fi
 
 feat="feat(feature):asdsad
 BREAKING"
 if  echo $feat | grep -qE '^feat'; then
                     echo true
-                    npm version major
+                    
 fi
 
